855b7c4d19b70c6836d4f990c700cadc68993ab7
zsquaredz <zhaozheng1997@hotmail.com>
2022-11-22 10:18:38 +0000

    run exp for getting representation for div10

--- scripts/preprocess_xlmr_base.sh
+++ scripts/preprocess_xlmr_base.sh
@@ -1,4 +1,14 @@
+#!/bin/bash
+HOME_DIR=/disk/ocean/zheng/ # Cohort-server
+source ${HOME_DIR}/.bashrc
+conda activate cdt
+
 for CORPUS in $(cat scripts/languages_common.lst); do
-  echo "python preprocess_treebank.py $CORPUS --xlmr xlm-roberta-base" --use-gpu
-  python preprocess_treebank.py $CORPUS --xlmr xlm-roberta-base --use-gpu
+  echo "python preprocess_treebank.py $CORPUS --xlmr xlm-roberta-base"
+  TRANSFORMERS_CACHE=${HOME_DIR}/.cache/
+  python preprocess_treebank.py $CORPUS \
+  --xlmr xlm-roberta-base \
+  --use-gpu \
+  --use_own_lm \
+  --model_path /disk/ocean/zheng/multilingual_lm_analysis/out/Div10_pretraining_output/final
 done
