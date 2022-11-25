cd81d9bf9327c7b8b8144f145c6e56894ebc2704
zsquaredz <zhaozheng1997@hotmail.com>
2022-11-25 12:22:17 +0000

    run exp for getting representation for div mono

--- scripts/preprocess_xlmr_base.sh
+++ scripts/preprocess_xlmr_base.sh
@@ -4,7 +4,7 @@ source ${HOME_DIR}/.bashrc
 conda activate cdt
 
 # # for multilingual models 
-# for line in $(cat scripts/languages_div10.lst); do
+# while read line; do
 #   CORPUS=($line)
 #   echo "python preprocess_treebank.py ${CORPUS[0]} --xlmr xlm-roberta-base"
 #   TRANSFORMERS_CACHE=${HOME_DIR}/.cache/
@@ -13,17 +13,16 @@ conda activate cdt
 #   --use-gpu \
 #   --use_own_lm \
 #   --model_path /disk/ocean/zheng/multilingual_lm_analysis/out/Div10_pretraining_output/final
-# done
+# done < scripts/languages_div10.lst
 
 # for monolingual models
 while read line; do
   CORPUS=($line)
-  echo ${CORPUS[0]} ${CORPUS[1]}
-  # echo "python preprocess_treebank.py ${CORPUS[0]} --roberta roberta-base"
-  # TRANSFORMERS_CACHE=${HOME_DIR}/.cache/
-  # python preprocess_treebank.py ${CORPUS[0]} \
-  # --roberta roberta-base \
-  # --use-gpu \
-  # --use_own_lm \
-  # --model_path /disk/ocean/zheng/multilingual_lm_analysis/out/${CORPUS[1]}_pretraining_output/final
+  echo "python preprocess_treebank.py ${CORPUS[0]} --roberta roberta-base"
+  TRANSFORMERS_CACHE=${HOME_DIR}/.cache/
+  python preprocess_treebank.py ${CORPUS[0]} \
+  --roberta roberta-base \
+  --use-gpu \
+  --use_own_lm \
+  --model_path /disk/ocean/zheng/multilingual_lm_analysis/out/${CORPUS[1]}_pretraining_output/final
 done < scripts/languages_div.lst
