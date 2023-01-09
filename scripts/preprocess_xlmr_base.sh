78d8a970fdc05add08aa6f04768e1374f3919835
zsquaredz <zhaozheng1997@hotmail.com>
2023-01-09 11:15:24 +0000

    run exp

--- scripts/preprocess_xlmr_base.sh
+++ scripts/preprocess_xlmr_base.sh
@@ -29,14 +29,27 @@ conda activate cdt
 #   --model_path /disk/ocean/zheng/multilingual_lm_analysis/out/Rel5_pretraining_output/final
 # done < scripts/languages_rel5.lst
 
-# for monolingual models
+# for multilingual models All-33
 while read line; do
   CORPUS=($line)
-  echo "python preprocess_treebank.py ${CORPUS[0]} --roberta roberta-base"
+  echo "python preprocess_treebank.py ${CORPUS[0]} --xlmr xlm-roberta-base"
   TRANSFORMERS_CACHE=${HOME_DIR}/.cache/
-  CUDA_VISIBLE_DEVICES=1 python preprocess_treebank.py ${CORPUS[0]} \
-  --roberta roberta-base \
+  CUDA_VISIBLE_DEVICES=0 python preprocess_treebank.py ${CORPUS[0]} \
+  --xlmr xlm-roberta-base \
   --use-gpu \
   --use_own_lm \
-  --model_path /disk/ocean/zheng/multilingual_lm_analysis/out/${CORPUS[1]}-multi-tok_pretraining_output/final
+  --exp_name All33 \
+  --model_path /disk/ocean/zheng/multilingual_lm_analysis/out/All33_pretraining_output/final
 done < scripts/languages_div.lst
+
+# # for monolingual models
+# while read line; do
+#   CORPUS=($line)
+#   echo "python preprocess_treebank.py ${CORPUS[0]} --roberta roberta-base"
+#   TRANSFORMERS_CACHE=${HOME_DIR}/.cache/
+#   CUDA_VISIBLE_DEVICES=1 python preprocess_treebank.py ${CORPUS[0]} \
+#   --roberta roberta-base \
+#   --use-gpu \
+#   --use_own_lm \
+#   --model_path /disk/ocean/zheng/multilingual_lm_analysis/out/${CORPUS[1]}-multi-tok_pretraining_output/final
+# done < scripts/languages_div.lst
