776aac7987cac914cb36adc18d4f67e7bf568551
zsquaredz <zhaozheng1997@hotmail.com>
2023-01-09 05:48:57 +0000

    debug

--- scripts/preprocess_xlmr_base.sh
+++ scripts/preprocess_xlmr_base.sh
@@ -3,18 +3,18 @@ HOME_DIR=/disk/ocean/zheng/ # Cohort-server
 source ${HOME_DIR}/.bashrc
 conda activate cdt
 
-# for multilingual models Div-10
-while read line; do
-  CORPUS=($line)
-  echo "python preprocess_treebank.py ${CORPUS[0]} --xlmr xlm-roberta-base"
-  TRANSFORMERS_CACHE=${HOME_DIR}/.cache/
-  python preprocess_treebank.py ${CORPUS[0]} \
-  --xlmr xlm-roberta-base \
-  --use-gpu \
-  --use_own_lm \
-  --exp_name Div10-CSD3 \
-  --model_path /disk/ocean/zheng/multilingual_lm_analysis/out/Div10_csd3_pretraining_output/final
-done < scripts/languages_div10.lst
+# # for multilingual models Div-10
+# while read line; do
+#   CORPUS=($line)
+#   echo "python preprocess_treebank.py ${CORPUS[0]} --xlmr xlm-roberta-base"
+#   TRANSFORMERS_CACHE=${HOME_DIR}/.cache/
+#   python preprocess_treebank.py ${CORPUS[0]} \
+#   --xlmr xlm-roberta-base \
+#   --use-gpu \
+#   --use_own_lm \
+#   --exp_name Div10-CSD3 \
+#   --model_path /disk/ocean/zheng/multilingual_lm_analysis/out/Div10_csd3_pretraining_output/final
+# done < scripts/languages_div10.lst
 
 # # for multilingual models Rel-5
 # while read line; do
@@ -29,14 +29,14 @@ done < scripts/languages_div10.lst
 #   --model_path /disk/ocean/zheng/multilingual_lm_analysis/out/Rel5_pretraining_output/final
 # done < scripts/languages_rel5.lst
 
-# # for monolingual models
-# while read line; do
-#   CORPUS=($line)
-#   echo "python preprocess_treebank.py ${CORPUS[0]} --roberta roberta-base"
-#   TRANSFORMERS_CACHE=${HOME_DIR}/.cache/
-#   CUDA_VISIBLE_DEVICES=1 python preprocess_treebank.py ${CORPUS[0]} \
-#   --roberta roberta-base \
-#   --use-gpu \
-#   --use_own_lm \
-#   --model_path /disk/ocean/zheng/multilingual_lm_analysis/out/${CORPUS[1]}-multi-tok_pretraining_output/final
-# done < scripts/languages_div.lst
+# for monolingual models
+while read line; do
+  CORPUS=($line)
+  echo "python preprocess_treebank.py ${CORPUS[0]} --roberta roberta-base"
+  TRANSFORMERS_CACHE=${HOME_DIR}/.cache/
+  CUDA_VISIBLE_DEVICES=1 python preprocess_treebank.py ${CORPUS[0]} \
+  --roberta roberta-base \
+  --use-gpu \
+  --use_own_lm \
+  --model_path /disk/ocean/zheng/multilingual_lm_analysis/out/${CORPUS[1]}-multi-tok_pretraining_output/final
+done < scripts/languages_div.lst
