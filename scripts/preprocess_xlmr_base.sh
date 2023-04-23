4823e0e809e6994424c341a4fca95c131ca12201
zsquaredz <zhaozheng1997@hotmail.com>
2023-04-23 22:09:07 +0100

    run exp all33 no transliterate

--- scripts/preprocess_xlmr_base.sh
+++ scripts/preprocess_xlmr_base.sh
@@ -29,28 +29,28 @@ conda activate cdt
 #   --model_path /disk/ocean/zheng/multilingual_lm_analysis/out/Rel5_pretraining_output/final
 # done < scripts/languages_rel5.lst
 
-# # for multilingual models All-33
-# while read line; do
-#   CORPUS=($line)
-#   echo "python preprocess_treebank.py ${CORPUS[0]} --xlmr xlm-roberta-base"
-#   TRANSFORMERS_CACHE=${HOME_DIR}/.cache/
-#   CUDA_VISIBLE_DEVICES=2 python preprocess_treebank.py ${CORPUS[0]} \
-#   --xlmr xlm-roberta-base \
-#   --use-gpu \
-#   --use_own_lm \
-#   --exp_name All33TRANS \
-#   --model_path /disk/ocean/zheng/multilingual_lm_analysis/out/All33_pretraining_output_transliterate/final
-# done < scripts/languages_debug.lst
-
-# for monolingual models
+# for multilingual models All-33
 while read line; do
   CORPUS=($line)
-  echo "python preprocess_treebank.py ${CORPUS[0]} --roberta roberta-base"
+  echo "python preprocess_treebank.py ${CORPUS[0]} --xlmr xlm-roberta-base"
   TRANSFORMERS_CACHE=${HOME_DIR}/.cache/
-  CUDA_VISIBLE_DEVICES=1 python preprocess_treebank.py ${CORPUS[0]} \
-  --roberta roberta-base \
+  CUDA_VISIBLE_DEVICES=2 python preprocess_treebank.py ${CORPUS[0]} \
+  --xlmr xlm-roberta-base \
   --use-gpu \
   --use_own_lm \
-  --exp_name TRANS \
-  --model_path /disk/ocean/zheng/multilingual_lm_analysis/out/${CORPUS[1]}-multi-tok_pretraining_output_transliterate/final
-done < scripts/languages_debug1.lst
+  --exp_name All33NOTRANS \
+  --model_path /disk/ocean/zheng/multilingual_lm_analysis/out/All33_pretraining_output/final
+done < scripts/languages_debug.lst
+
+# # for monolingual models
+# while read line; do
+#   CORPUS=($line)
+#   echo "python preprocess_treebank.py ${CORPUS[0]} --roberta roberta-base"
+#   TRANSFORMERS_CACHE=${HOME_DIR}/.cache/
+#   CUDA_VISIBLE_DEVICES=1 python preprocess_treebank.py ${CORPUS[0]} \
+#   --roberta roberta-base \
+#   --use-gpu \
+#   --use_own_lm \
+#   --exp_name TRANS \
+#   --model_path /disk/ocean/zheng/multilingual_lm_analysis/out/${CORPUS[1]}-multi-tok_pretraining_output_transliterate/final
+# done < scripts/languages_debug1.lst
