8e0f4808d29e78356a40a72498b2c13ce917cef6
zsquaredz <zhaozheng1997@hotmail.com>
2023-05-17 12:42:59 +0100

    run exp for low resource fr

--- scripts/preprocess_xlmr_base.sh
+++ scripts/preprocess_xlmr_base.sh
@@ -34,14 +34,14 @@ while read line; do
   CORPUS=($line)
   echo "python preprocess_treebank.py ${CORPUS[0]} --xlmr xlm-roberta-base"
   TRANSFORMERS_CACHE=${HOME_DIR}/.cache/
-  CUDA_VISIBLE_DEVICES=0 python preprocess_treebank.py ${CORPUS[0]} \
+  CUDA_VISIBLE_DEVICES=1 python preprocess_treebank.py ${CORPUS[0]} \
   --xlmr xlm-roberta-base \
   --use-gpu \
   --use_own_lm \
-  --exp_name All33LowEN \
+  --exp_name All33LowFR \
   --lang ${CORPUS[1]} \
   --transliterate \
-  --model_path /disk/ocean/zheng/multilingual_lm_analysis/out/All33_pretraining_output_low_en/final
+  --model_path /disk/ocean/zheng/multilingual_lm_analysis/out/All33_pretraining_output_low_fr/final
 done < scripts/languages_debug.lst
 
 # # for monolingual models
