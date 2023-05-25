ed3c2f798a9268677d431a8a4ae5905a652f8703
zsquaredz <zhaozheng1997@hotmail.com>
2023-05-26 00:23:08 +0100

    run low tr

--- scripts/preprocess_xlmr_base.sh
+++ scripts/preprocess_xlmr_base.sh
@@ -36,13 +36,13 @@ while read line; do
   CORPUS=($line)
   echo "python preprocess_treebank.py ${CORPUS[0]} --xlmr xlm-roberta-base"
   TRANSFORMERS_CACHE=${HOME_DIR}/.cache/
-  CUDA_VISIBLE_DEVICES=1 python preprocess_treebank.py ${CORPUS[0]} \
+  CUDA_VISIBLE_DEVICES=2 python preprocess_treebank.py ${CORPUS[0]} \
   --xlmr xlm-roberta-base \
   --use-gpu \
   --use_own_lm \
-  --exp_name All33LowVI \
+  --exp_name All33LowTR \
   --lang ${CORPUS[1]} \
-  --model_path /disk/ocean/zheng/multilingual_lm_analysis/out/All33_pretraining_output_low_vi/final
+  --model_path /disk/ocean/zheng/multilingual_lm_analysis/out/All33_pretraining_output_low_tr/final
 done < scripts/languages_debug.lst
 
 # # for pretrained checkpoint multilingual models All-33
