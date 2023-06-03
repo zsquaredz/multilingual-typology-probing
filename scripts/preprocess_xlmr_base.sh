19cc34a09d1db325f1c39f745c1f2cb7e8823097
zsquaredz <zhaozheng1997@hotmail.com>
2023-06-03 12:25:38 +0100

    run debug

--- scripts/preprocess_xlmr_base.sh
+++ scripts/preprocess_xlmr_base.sh
@@ -40,9 +40,9 @@ while read line; do
   --xlmr xlm-roberta-base \
   --use-gpu \
   --use_own_lm \
-  --exp_name All33LowFR10 \
+  --exp_name All33LowKO10 \
   --lang ${CORPUS[1]} \
-  --model_path /disk/ocean/zheng/multilingual_lm_analysis/out/All33_pretraining_output_low_fr_10/final
+  --model_path /disk/ocean/zheng/multilingual_lm_analysis/out/All33_pretraining_output_low_ko_10/final
 done < scripts/languages_debug2.lst
 
 # # for pretrained checkpoint multilingual models All-33
